a9ee8c1603f041af20df4d9a272025c29af1b994
Glitch (hello-express) <none>
2020-06-09 05:14:27 +0000

    🙅⏰ Checkpoint ./package.json:2310301/2850 ./server.js:2310301/447

--- server.js
+++ server.js
@@ -3,13 +3,21 @@
 const express = require('express');
 const body_parser =  require('body-parser');
 const axios = require('axios').default;
+const uuid = require('uuid');
+const Database = require('better-sqlite3');
+
+const db = new Database('twitch.db');
+db.run(`CREATE TABLE IF NOT EXISTS LoginSessions`)
 const app = express();
 const REDIRECT_URI='https://gm-twitch-oauth.glitch.me/callback'
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.type('text').send(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=https://gm-twitch-oauth.glitch.me/callback&response_type=code&scope=user:read:email`);
+});
+app.get('/login',(req,res)=>{
+  const stmt = db.prepare(`INSERT INTO`)
 });
 
 app.get('/callback',async (req,res)=>{
