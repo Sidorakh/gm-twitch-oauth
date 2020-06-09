4fa9fa26b45b0c91e3d858c60f6b29bdbade9023
Glitch (hello-express) <none>
2020-06-09 05:26:33 +0000

    🎠💏 Checkpoint ./server.js:2310301/1681

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ const express = require('express');
 const body_parser =  require('body-parser');
 const axios = require('axios').default;
 const uuid = require('uuid');
-const Database = require('better-sqlite3');
+//const Database = require('better-sqlite3');
 
-const db = new Database('twitch.db');
-db.run(`CREATE TABLE IF NOT EXISTS LoginSessions`)
+//const db = new Database('twitch.db');
+//db.prepare(`CREATE TABLE IF NOT EXISTS LoginSessions(Token TEXT PRIMARY KEY)`).run();
+const sessions = {};
 const app = express();
 const REDIRECT_URI='https://gm-twitch-oauth.glitch.me/callback'
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
-  res.type('text').send(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=https://gm-twitch-oauth.glitch.me/callback&response_type=code&scope=user:read:email`);
+  res.send(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=https://gm-twitch-oauth.glitch.me/callback&response_type=code&scope=user:read:email`);
 });
 app.get('/login',(req,res)=>{
-  const stmt = db.prepare(`INSERT INTO`)
+  let session_id = uuid.v4();
+  sessions[session_id] = {
+    expiry: setTimeout(()=>{
+      delete sessions[session_id];
+    },1000*120)// 2 min timeout
+  };
+  res.json({
+    token:session_id,
+    url:`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=https://gm-twitch-oauth.glitch.me/callback&response_type=code&scope=user:read:email&state=${session_id}`
+  });
 });
 
 app.get('/callback',async (req,res)=>{
@@ -28,4 +38,4 @@ app.get('/callback',async (req,res)=>{
 })
 
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT,()=>{console.log(`Live on port ${process.env.PORT}`)});
