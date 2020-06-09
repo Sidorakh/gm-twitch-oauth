dd32dff1505f17415e8a746486aa34752d384404
Glitch (hello-express) <none>
2020-06-09 04:40:17 +0000

    👛🍩 Checkpoint ./server.js:2310301/1395

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const express = require('express');
 const body_parser =  require('body-parser');
 const axios = require('axios').default;
 const app = express();
-const REDIRECT_URI='https://gm-twitch-oauth.me/callback'
+const REDIRECT_URI='https://gm-twitch-oauth.glitch.me/callback'
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
 
 app.get("/", (req, res) => {
-  res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=http://localhost:3000/callback&response_type=code&scope=user:read:email`);
+  res.type('text').send(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=https://gm-twitch-oauth.glitch.me/callback&response_type=code&scope=user:read:email`);
 });
 
 app.get('/callback',async (req,res)=>{
-  const result = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}&client_secre=${process.env.CLIENT_SECRET}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}`);
+  const result = await axios.post(  `https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}`
+                                  + `&client_secret=${process.env.SECRET_ID}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}`,
+{},{validateStatus:false});
   res.json(result.data)
 })
 
